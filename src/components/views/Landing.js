ba35ba6d2adc55aa5f9f6021533aa4e98b4161b8
Emas <emas.dev@gmail.com>
2021-10-06 15:19:13 -0500

    add right panel login

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -1,6 +1,16 @@
 import React from "react";
-import Layout from "../Layout/Layout";
-import { Grid, Box, Image, Center, Text } from "@chakra-ui/react";
+import {
+  Grid,
+  Box,
+  Image,
+  Center,
+  Text,
+  VStack,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+
+import LoginForm from "../auth/LoginForm";
 import { useAuth } from "../../hooks/useAuth";
 
 import heroImg from "../../img/hero.png";
@@ -9,15 +19,31 @@ const Landing = () => {
   const { user, userData } = useAuth();
   return (
     <Grid templateColumns="repeat(2, 1fr)" minH="100vh">
-      <Box w="100%" bg="blue.600" >
-        <Center h="100%" padding={8}>
-          <Box>
-            <Image src={heroImg} />
-          </Box>
+      <Box w="100%" bg="blue.800">
+        <Center h="100%">
+          <VStack spacing={4}>
+            <Box>
+              <Image w="70%" m="auto" src={heroImg} />
+            </Box>
+            <Box>
+              <Text color="blue.100" mt={4}>
+                En IDM puedes agendar cita para los estudios de tu paciente y
+                más
+              </Text>
+            </Box>
+          </VStack>
         </Center>
-
       </Box>
-      <Box w="100%" bg="blue.200" />
+      <Box w="100%" bg="blue.100">
+        <Grid templateRows="100px 1fr" minH="100vh">
+          <Flex justifyContent="flex-end">
+            <Button m={4}>Ingresar</Button>
+          </Flex>
+          <Box>
+            <LoginForm />
+          </Box>
+        </Grid>
+      </Box>
     </Grid>
   );
 };
